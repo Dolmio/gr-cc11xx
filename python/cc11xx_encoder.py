cea7d01c10f50595dac74c1592aec275f1408a89
Juho Salmio <juho.salmio@reaktor.com>
2016-02-15 13:01:17 +0000

    add cc11xx encoder block, which inputs and outputs messages

---
+++ python/cc11xx_encoder.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2016 <+YOU OR YOUR COMPANY+>.
+# 
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+import numpy
+from gnuradio import gr
+import pmt
+import cc11xx_packet
+class cc11xx_encoder(gr.basic_block):
+    def __init__(self, preamble=[0x20, 0x20, 0x20, 0x20, 0x20, 0x20], syncword=[0x35, 0x2E, 0x35, 0x2E], crc=True, whitening=True):
+        gr.basic_block.__init__(self,
+            name="cc11xx-encoder",
+            in_sig=[],
+            out_sig=[])
+        self.message_port_register_out(pmt.intern('out'))
+        self.message_port_register_in(pmt.intern('in'))
+        self.set_msg_handler(pmt.intern('in'), self.handle_msg)
+
+        self.syncword = syncword
+        self.preamble = preamble
+        self.crc = crc
+        self.whitening = whitening
+
+    def handle_msg(self, msg_pmt):
+        msg_str = pmt.to_python(pmt.cdr(msg_pmt))
+        packet_buffer = cc11xx_packet.create(msg_str, preamble= self.preamble, syncword=self.syncword, crc=self.crc, whitening=self.whitening)
+        packet_arr = numpy.frombuffer(packet_buffer, dtype='uint8')
+        self.message_port_pub(pmt.intern('out'), pmt.cons(pmt.PMT_NIL, pmt.to_pmt(packet_arr)))
--- python/qa_cc11xx_deframer_bb.py
+++ python/qa_cc11xx_deframer_bb.py
@@ -1,28 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# 
-# Copyright 2014 <+YOU OR YOUR COMPANY+>.
-# 
-# This is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3, or (at your option)
-# any later version.
-# 
-# This software is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this software; see the file COPYING.  If not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street,
-# Boston, MA 02110-1301, USA.
-# 
-
-from gnuradio import gr, gr_unittest
-from gnuradio import blocks
+from gnuradio import gr, gr_unittest, digital, blocks
 import cc11xx_swig as cc11xx
-
+import numpy as np
+import pmt
+import cc11xx_encoder
+import time
 class qa_cc11xx_deframer_bb (gr_unittest.TestCase):
 
     def setUp (self):
@@ -31,11 +12,50 @@ class qa_cc11xx_deframer_bb (gr_unittest.TestCase):
     def tearDown (self):
         self.tb = None
 
-    def test_001_t (self):
-        # set up fg
-        self.tb.run ()
-        # check data
+    def encode_decode_test(self, payload_str="TEST", whitening=False, encode_crc=False, decode_crc=False):
+        preamble = "01010101"
+        sync1 = 0x2
+        sync2 = 0x3
+        sync_length = 2
+        payload = [ord(c) for c in payload_str]
 
+        strobe = blocks.message_strobe(pmt.cons(pmt.PMT_NIL, pmt.to_pmt(list(payload))), 200)
+        encoder = cc11xx_encoder.cc11xx_encoder(preamble=[int(preamble, 2)], syncword=[sync1, sync2], whitening=whitening, crc=encode_crc)
+        pdu_to_stream = blocks.pdu_to_tagged_stream(blocks.byte_t, "packet_len")
+        debug = blocks.message_debug()
+        self.tb.msg_connect(strobe, "strobe", encoder, "in")
+        self.tb.msg_connect(encoder, "out", pdu_to_stream, "pdus")
+
+        unpack = blocks.packed_to_unpacked_bb(1, 0)
+
+        acc_code_block =  digital.correlate_access_code_tag_bb(preamble, 0, "preamble")
+        deframer = cc11xx.cc11xx_deframer_bb(sync1 ,sync2, whitening, decode_crc, sync_length)
+
+        self.tb.connect(pdu_to_stream,unpack)
+        self.tb.connect(unpack, acc_code_block)
+        self.tb.connect(acc_code_block, deframer)
+        self.tb.msg_connect((deframer, 'out'), (debug, 'store'))
+        self.tb.start()
+        time.sleep(1)
+        self.tb.stop()
+        #Please get rid of this sleep if you know how!
+        time.sleep(0.1)
+        self.tb.stop()
+
+        result_data = [i for i in pmt.to_python(pmt.cdr(debug.get_message(0)))]
+        self.assertEqual(payload, result_data)
+
+    def test_encode_decode_without_whitening (self):
+        self.encode_decode_test(payload_str="TEST", whitening=False)
+
+    def test_encode_decode_with_long_payload (self):
+        self.encode_decode_test(payload_str="T" * 255, whitening=False)
+
+    def test_encode_decode_with_whitening (self):
+        self.encode_decode_test(payload_str="TEST", whitening=True)
+
+    def test_encode_decode_with_crc_in_encode (self):
+        self.encode_decode_test(payload_str="TEST", whitening=False, encode_crc=True)
 
 if __name__ == '__main__':
-    gr_unittest.run(qa_cc11xx_deframer_bb, "qa_cc11xx_deframer_bb.xml")
+    gr_unittest.main ()
